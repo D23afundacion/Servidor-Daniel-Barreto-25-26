b8523c1e1cdc19f5a30f1c967b795564305c6487
D23afundacion <dbarretom23@fpcoruna.afundacion.org>
2025-10-07 23:44:43 +0200

    Sprint1.2 Ej5

---
+++ sprint1apache/www/site3/temperaturas.php
@@ -0,0 +1,34 @@
+<html>
+    <body>
+        <h1>Celsius y Fahrenheit</h1>
+        <p>Convierte de celsius a fahrenheit o viceversa</p>
+        <p>
+        <?php
+            if (isset($_POST["ftransformacion"])) {
+                if ($_POST["ftransformacion"] == "celsius") {
+                    $cantidad = $_POST["fcantidad"];
+                    $transformado = ($cantidad * (9 / 5)) + 32;
+                    echo "Resultado: ".$cantidad." oC = ".$transformado." oF";
+                } else if ($_POST["ftransformacion"] == "fahrenheit") {
+                    $cantidad = $_POST["fcantidad"];
+                    $transformado = (($cantidad - 32) * 5 ) / 9;
+                    echo "Resultado: ".$cantidad." oF = ".$transformado." oC";
+                } else {
+                    echo "Resultado: ...";
+                }
+            }
+        ?>
+        </p>
+        <p>Realiza la conversión:</p>
+        <form action="/temperaturas.php" method="post">
+            <label for="cantidad_input">Cantidad:</label>
+            <br>
+            <input type="number" id="cantidad_input" name="fcantidad"><br>
+            <input type="radio" id="celsius_input" name="ftransformacion" value="celsius">
+            <label for="celsius_input">Celsius</label><br>
+            <input type="radio" id="fahrenheit_input" name="ftransformacion" value="fahrenheit">
+            <label for="fahrenheit_input">Fahrenheit</label><br>
+            <input type="submit" value="Convertir">
+        </form>
+    </body>
+</html>
