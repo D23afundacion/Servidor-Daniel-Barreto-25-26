aa3eb4d23dd4691c6a8ac9e6c3208ad93820af55
D23afundacion <dbarretom23@fpcoruna.afundacion.org>
2025-10-29 13:12:32 +0100

    Sprint2 Ej4

--- sprint2phpsql/www/mysite/detail.php
+++ sprint2phpsql/www/mysite/detail.php
@@ -38,5 +38,11 @@
             mysqli_close($db);
         ?>
     </ul>
+    <p>Deja un nuevo comentario:</p>
+    <form action="/comment.php" method="post">
+        <textarea rows="4" cols="50" name="new_comment"></textarea><br>
+        <input type="hidden" name="id_coche" value="<?php echo $coche_id; ?>">
+        <input type="submit" value="Comentar">
+    </form>
 </body>
 </html>
